05c17ac01f9d9d8b9785b1313ec2d184b00cf9e4
Eden S.-G <edgro2693@gmail.com>
2014-04-18 19:50:52 -0400

    mobile-friendly updates, new menu
    also deleted unused file

--- js/jquery.onepage-scroll.js
+++ js/jquery.onepage-scroll.js
@@ -267,8 +267,8 @@
     // Create Pagination and Display Them
     if(settings.pagination == true) {
       $("<ul class='onepage-pagination'>" + paginationList + "</ul>").prependTo("body");
-      posTop = (el.find(".onepage-pagination").height() / 2) * -1;
-      el.find(".onepage-pagination").css("margin-top", posTop);
+      //posTop = (el.find(".onepage-pagination").height() / 2) * -1;
+      //el.find(".onepage-pagination").css("margin-top", posTop);
     }
     
     if(window.location.hash != "" && window.location.hash != "#1") {
